618ab364c1e0a2291a77ccfd9e74eae193fc56d7
Gary White <glw494@gmail.com>
2024-03-16 17:28:54 -0700

    CORS config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,23 @@
-from typing import Annotated
-
+import os
 from fastapi import FastAPI, Security
+from starlette.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from .utils import VerifyToken
 
 app = FastAPI()
 auth = VerifyToken()
 
+origins = [
+    os.getenv("LOCAL_ORIGIN"), os.getenv("DEPLOYED_ORIGIN")
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/public")
 def public():
